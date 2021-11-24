89a5b06195c2feb3b3573b55e67416f450174f9d
Avnish2010 <78597045+Avnish2010@users.noreply.github.com>
2021-11-24 19:50:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+
+// Create canvas variable
+var canvas = new fabric.Canvas('myCanvas') ;
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
+	function new_image(get_image)
+}
+fabric.Image.fromURL(get_image, function(Img) {
+	block_image_object = Img;
+
+
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_object);
+	block_image_object.set})
+	top:block_y,
+	leftblock_x
+canvas.add(block_image_object);
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '?') // add appropriate keycode
+	{
+		// upload red ranger
+	}
+	if(keyPressed == '71')
+	{
+		new_image(rr1.png)
+		console.log("r")
+	}
+		block_x = 200;
+		// upload green ranger
+	}
+	
+	if(keyPressed == '89')
+	{
+		new_image(gr1.png)
+		console.log("s")
+	}
+	{
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		new_image(yr1.png)
+		console.log("d")
+	}
+	{
+		block_x = 600;
+		// upload pink ranger
+		{
+			new_image(pr1.png)
+			console.log("f")
+		}
+	}
+	if(keyPressed == '66')
+	{
+		new_image(br1.png)
+		console.log("q")
+	}
+	
+	
+	{
+		block_x = 700;
+	// upload blue ranger
+	
+	}
+	
+
